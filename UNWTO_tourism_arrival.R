ca6b2069d47876586b57184ee3a9866138f70ea3
Calvin de Wilde <58243990+calvindw@users.noreply.github.com>
2020-05-02 13:51:27 +0700

    Update UNWTO_tourism_arrival.R

--- UNWTO_tourism_arrival.R
+++ UNWTO_tourism_arrival.R
@@ -1,5 +1,7 @@
 #this code will transform UNWTO data hosted on data.un.org into tidy data you can use for visualizations
-#this code will focus on tourist arrivals data
+#this code will focus on inbound tourism ("Arrivals - Thousands") and outbound tourism ("Departures - Thousands") variables
+#inbound tourism and outbound tourism are originated from three sources (IMF, VF, TF), we will choose the maximum value based on those three sources
+#I haven't checked the rest of variables other than those two, so please recheck if you decide to use them.
 
 
 #Load Libraries----
@@ -49,11 +51,11 @@ vars_to_replace <- c("Arrivals - Thousands",
                      "Tourism expenditure in other countries - US$ Mn",
                      "Source: World Tourism Organization (UNWTO)")
 
-#clean the rows of Country before using lacf
+#clean the rows of country before using locf
 df <- df %>% mutate(country = case_when(country %in% vars_to_replace ~ NA_character_, 
                                         TRUE ~ country))
 
-#use na.locf to fill the blanks at Country column using na.locf
+#use na.locf to fill the blanks at country column using na.locf
 df <- df %>% mutate_at(vars(country), funs(na.locf))
 
 #use distinct() to make a filter
@@ -65,14 +67,14 @@ df <- df %>% filter(!str_detect(variables, paste(mask, collapse = "|")))
 #unpivot
 df<- df %>% gather(-country, -series,-variables, key="year", value="value")
 
-#clean year column (x1995, x1996 which were introduced by janitor)
+#clean year column (x1995, x1996, etc, which were introduced by janitor)
 vars_to_replace = c("x" = "")
 df <- df %>% mutate_at(vars(year), str_replace_all, pattern=vars_to_replace)
 
-#change Value column as numeric and year as integer
+#change value column as numeric and year as integer
 df <- df %>% mutate(value = as.numeric(value), year = as.integer(year))
 
-#convert misc characters into  to zeros
+#convert NA characters to zeros
 df <- df %>%  replace(., is.na(.), "0")
 
 #convert value column into numeric
@@ -80,7 +82,6 @@ df <- df %>% mutate_at(vars(value), as.numeric)
 
 #change country, variables, and series into factor
 df <- df %>% mutate_at(vars(country,variables,series), as_factor)
-
 
 #impute missing values by selecting the maximum value from group of varibles
 #we only use arrivals and departures by reconcile different data from the series (IMF, TF, VF)
@@ -91,13 +92,14 @@ df <- df %>%
   filter(value == max(value))
 
 
-#pull data from world bank data we will use for lef_join reference (uplosded to my github for your convenience)
+#pull data from world bank data we will use for left_join reference (uploaded to my github for your convenience)
 db <- read.csv2(url("https://raw.githubusercontent.com/calvindw/datasets/master/countries.csv"), stringsAsFactors =FALSE)
 
 #transform the iso2c column from db to lowercase so it can be used for left_join with country_code
 db <- db %>% mutate(iso2c = tolower(iso2c))
 
 #filter arrival data
+#if you want to see the departures, change "Arrivals - Thousands" into "Departures - Thousands".
 df_arrival <- df %>% dplyr::filter(str_detect(variables, "Arrivals - Thousands"))
 
 #### Data cleaning for reverse bar animation with geom_bar (arrivals) ####
@@ -132,7 +134,7 @@ static_plot <- df_arrival %>%
   ggplot(.) +
   aes(x=rank, group=country, fill=country)+
   geom_bar(aes(y=value, fill=country, group=country), stat="identity")+
-  geom_text(aes(y = 0, label = country, hjust = 1), size=5)+
+  geom_text(aes(y = 0, label = country, hjust = 1), size=5)+  
   geom_text(aes(y = max(value)/2, label = scales::comma(value)), size = 5)+
   geom_flag(aes(y = max(value)/10, country=iso2c))+ 
   scale_x_reverse()+
